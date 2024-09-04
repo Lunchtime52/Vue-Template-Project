aa83e8e69a9093d414ca0f0af752cc951d66f2bc
Ryan <ryanbuchanan52.tn@gmail.com>
2024-09-04 13:01:07 -0400

    added reset css

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,21 @@
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
+/* Reset CSS */
+h1,h2,h3,h4,h5,h6{
+  text-wrap: balance;
+}
+p,li,figcaption{
+  max-width: 65ch;
+  text-wrap: pretty;
+}
+
+/* 
+
+header, footer, main, section, article{
+container-type: inline-size;
+}
+
+*/
 </style>
