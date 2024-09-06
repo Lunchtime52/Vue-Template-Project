95efb0cb99a90c86a20cd36b3e05caa2b19197f3
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2024-09-06 05:11:23 +0000

    added 404 page

--- src/router.js
+++ src/router.js
@@ -5,11 +5,14 @@ const routes = [
   { path: "/", component: () => import("./pages/Home.vue") ,name:"Home" },
   { path: "/contact", component: () => import("./pages/Contact.vue"),name:"Contact" },
 //   { path: "/services", component: () => import("./pages/Services.vue") },
+{ path: '/:pathMatch(.*)*', name: 'NotFound', component: ()=>import('./components/NotFound.vue') }
+
 ];
 
 const router = createRouter({
   history: createWebHistory(base),
   routes,
 });
+
 
 export default router;
