cc79fde0af8daa593c75b307e863f153ecebdbf6
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-03-19 16:59:04 +0000

    Fixing Github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { BootstrapVueNextResolver } from "bootstrap-vue-next";
 
+const base = '/Vue-Template-Project/'
+
 export default defineConfig({
   plugins: [
     vue(),
