d3233c015c72e4ed59d567344115813c41d2596d
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-03-19 22:15:01 +0000

    Deployment is working.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,13 @@
 <template>
   <BNavbar toggable="md" :type="variant" :variant="color" class="px-4 mb-2">
-    
     <BNavbarBrand :href="logoHref">{{ logoText }}</BNavbarBrand>
     <BNavbarNav>
-      <BNavItem v-for="link in navLinks" :key="link.href" :to="base.concat(link.page)">{{
-        link.text
-      }}</BNavItem>
+      <BNavItem
+        v-for="link in navLinks"
+        :key="link.href"
+        :to="base.concat(link.page)"
+        >{{ link.text }}</BNavItem
+      >
     </BNavbarNav>
   </BNavbar>
 </template>
@@ -18,7 +20,7 @@ export default {
     navLinks: { type: Array, default: () => [] },
     variant: { type: String, default: "dark" },
     color: { type: String, default: "info" },
-    base:{type:String,default:"/Vue-Template-Project"}
+    base: { type: String, default: "/Vue-Template-Project" },
   },
 };
 </script>
