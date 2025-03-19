449accb427a9fe8ebfb359bca2a72824295c1af9
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-03-19 18:00:53 +0000

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { BootstrapVueNextResolver } from "bootstrap-vue-next";
 
-const base = '/Vue-Template-Project/'
+// const base = '/Vue-Template-Project/'
 
 export default defineConfig({
+  base:'/Vue-Template-Project/',
   plugins: [
     vue(),
     Components({
