a2216a5ab813153a4acf53c42f58c7f7b0c52bd0
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-07-11 00:02:27 +0000

    fix2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <!-- @format -->
 
 <template>
-	<!-- <Navbar
+	<Navbar
 		logoText="My Awesome Site"
 		:navLinks="[
 			{ text: 'Home', href: 'Home' },
 			{ text: 'About', href: 'About' },
 			{ text: 'Contact', href: 'Contact' },
-		]" /> -->
+		]"
+	/>
 
-	<!-- <router-view></router-view> -->
+	<router-view></router-view>
 
 	<Footer
 		:footerLinks="[
 			// { text: 'Privacy', href: '/privacy' },
 			// { text: 'Terms', href: '/terms' },
-		]" />
+		]"
+	/>
 </template>
 
 <style src="./assets/custom.scss"></style>
